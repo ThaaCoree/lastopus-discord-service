adcc79a66511bd3276deef5d2c53763ebb29ee26
ThaaCoree <jaruwith2710@gmail.com>
2026-05-16 21:39:38 +0700

    rune giving bug fix

--- src/main/java/model/entity/units/Unit.java
+++ src/main/java/model/entity/units/Unit.java
@@ -926,7 +926,7 @@ public class Unit {
 
     public Rune findRune(int index) {
         Map<Integer, Rune> reindexed = new LinkedHashMap<>();
-        int i = 0;
+        int i = 1;
         for (Rune rune_reindex : rune_inventory.values()) {
             reindexed.put(i++, rune_reindex);
         }
