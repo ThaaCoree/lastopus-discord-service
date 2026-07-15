5a365d12b2fd26358628758ec375a68907897506
ThaaCoree <jaruwith2710@gmail.com>
2026-07-15 18:24:11 +0700

    patch 7 backpack inventory

--- src/main/java/app/Database.java
+++ src/main/java/app/Database.java
@@ -106,6 +106,7 @@ public class Database {
 
     public void loadItemFromJson() {
 //        allPlayerMap = JsonUtils.loadFromFile("/json/players.json", new TypeReference<Map<String, Unit>>() {});
+        allNPCMap = JsonUtils.loadFromFile("/json/npcs.json", new TypeReference<Map<String, Unit>>() {});
 //        allMonsterMap = JsonUtils.loadFromFile("/json/monsters.json", new TypeReference<Map<String, Monster>>() {});
 //        allCardMap = JsonUtils.loadFromFile("/json/cards.json", new TypeReference<Map<String, Card>>() {});
 //        allConditionMap = JsonUtils.loadFromFile("/json/conditions.json", new TypeReference<Map<String, Conditions>>() {});
